b8ed840fa8612f8ddfe5fd154b080b74fb599aa6
Bigboss619 <eugochukwu910@gmail.com>
2026-07-14 23:05:35 +0100

    latest refactor

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup');
 const connectDB = require('./config/db');
 const keys = require('./config/keys');
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 // set up routes
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 // create home route
 app.get('/', (req, res) => {
